eb5569ba782a0502b57849b385249dcbbb21418c
Big Cheesy B <Big Cheesy B@MemeMachine>
2016-10-19 00:16:08 +0100

    Comments can now be displayed

--- src/main/java/uk/ac/dundee/computing/aec/instagrim/servlets/Comments.java
+++ src/main/java/uk/ac/dundee/computing/aec/instagrim/servlets/Comments.java
@@ -17,7 +17,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.LinkedList;
 import java.util.UUID;
-import java.math.BigInteger;
 import uk.ac.dundee.computing.aec.instagrim.lib.CassandraHosts;
 import uk.ac.dundee.computing.aec.instagrim.lib.Convertors;
 import uk.ac.dundee.computing.aec.instagrim.stores.Comment;
@@ -29,10 +28,14 @@ import uk.ac.dundee.computing.aec.instagrim.models.CommentModel;
  *
  * @author Big Cheesy B
  */
-@WebServlet(name = "Comments", urlPatterns = {"/Comments","/Comments/*"})
+@WebServlet(name = "postComment", urlPatterns = {"/Comments","/Comments/*"})
 public class Comments extends HttpServlet {
     
     private Cluster cluster = null;
+    
+    public Comments(){
+        
+    }
     
     public void init(ServletConfig config) throws ServletException {
         cluster = CassandraHosts.getCluster();
@@ -43,31 +46,25 @@ public class Comments extends HttpServlet {
         
         //Splits up the url into an array of strings with / delimiter
         String args[] = Convertors.SplitRequestPath(request);
-        if(args[2] == "comments.jsp"){
-        }
-        else{
-            System.out.println(args[2]);
-            //Extracts the image's id from the args
-            UUID picid = UUID.fromString(args[2]);
+        displayComments(args[2], request, response);
+    }
+    
+    public void displayComments(String picID, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{       
+            UUID uuid = UUID.fromString(picID);
 
             CommentModel cm = new CommentModel();
             cm.setCluster(cluster);
+            LinkedList<UUID> lsCommentIDs;
             LinkedList<Comment> lsComments;
 
-            lsComments = cm.getCommentsFromPic(picid);
+            lsCommentIDs = cm.getCommentsFromPic(uuid);
+            lsComments = cm.getCommentsFromID(lsCommentIDs);
 
+            RequestDispatcher rd = request.getRequestDispatcher("/comments.jsp");
             //Sets request attribute to the Linked List of comments ids for the image
             request.setAttribute("comments", lsComments);
-            RequestDispatcher rd = request.getRequestDispatcher("comments.jsp");
-            rd.forward(request, response);
-        }
-        
+            rd.forward(request, response);     
     }
     
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        
-        
-    }
         
 }
